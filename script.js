3437b348771669de8f9b5d1800dd3fcdee5f527b
KasthuriRangan <krangan415@gmail.com>
2023-08-21 19:14:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 //your JS code here. If required.
+function secondHighest(arr) {
+    if (arr.length <= 1 || arr.every((e) => e === arr[0])) {
+        return -Infinity;
+    } else {
+        arr.sort((a, b) => b - a);
+        return arr[1];
+    }
+}
+
+function Main() {
+    var n = prompt("Enter the number of elements");
+    var arr = [];
+    for (var i = 0; i < n; i++) {
+        arr[i] = Number(prompt("Enter element " + (i + 1)));
+    }
+    alert(secondHighest(arr));
+}
+Main();
